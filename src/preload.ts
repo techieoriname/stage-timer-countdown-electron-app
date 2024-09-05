52885d8b092742b9e994146a1dc781837de58740
techieoriname <techieoriname@gmail.com>
2024-09-05 12:03:42 +0100

    update exposed functions

--- src/preload.ts
+++ src/preload.ts
@@ -1,20 +1,20 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electron', {
-    startTimer: (time: number) => ipcRenderer.send('start-timer', { time }),
+    startTimer: (time: number, activity: string) => ipcRenderer.send('start-timer', { time, activity }),
     resetTimer: () => ipcRenderer.send('reset-timer'),
     setFlashState: (state: boolean) => ipcRenderer.send('set-flash-state', state),
-    sendTimeUpdate: (time: number) => ipcRenderer.send('time-update', time), // Add this line
-    onStartTimer: (callback: (event: any, { time }: { time: number }) => void) => ipcRenderer.on('start-timer', (event, { time }) => {
-        callback(event, { time });
+    sendTimeUpdate: (time: number) => ipcRenderer.send('time-update', time),
+    onStartTimer: (callback: (event: Electron.IpcRendererEvent, { time, activity }: { time: number, activity: string }) => void) => ipcRenderer.on('start-timer', (event, { time, activity }) => {
+        callback(event, { time, activity });
     }),
     onResetTimer: (callback: () => void) => ipcRenderer.on('reset-timer', () => {
         callback();
     }),
-    onFlashStateChange: (callback: (event: any, state: boolean) => void) => ipcRenderer.on('set-flash-state', (event, state) => {
+    onFlashStateChange: (callback: (event: Electron.IpcRendererEvent, state: boolean) => void) => ipcRenderer.on('set-flash-state', (event, state) => {
         callback(event, state);
     }),
-    onTimeUpdate: (callback: (event: any, time: number) => void) => ipcRenderer.on('time-update', (event, time) => {
+    onTimeUpdate: (callback: (event: Electron.IpcRendererEvent, time: number) => void) => ipcRenderer.on('time-update', (event, time) => {
         callback(event, time);
     }),
 });
